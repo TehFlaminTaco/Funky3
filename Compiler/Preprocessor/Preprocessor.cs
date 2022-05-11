99faa81a625357cb2fbbff4ac840f9c9a9ee6af9
William Lemon <William.Lemon2@gmail.com>
2022-05-12 09:57:57 +1000

    Preprocessor now better handles strings and comments
    No more replacing their contents with defines

--- Compiler/Preprocessor/Preprocessor.cs
+++ Compiler/Preprocessor/Preprocessor.cs
@@ -2,14 +2,27 @@ using System.Text.RegularExpressions;
 using System.Text;
 public class Preprocessor {
     public static readonly Regex IsDirective = new(@"^\s*\$(?<directive>\w+)(?<body>.*)$");
+
+    // This is used to quickly chunk up the code for preprocessing.
+    public static readonly Regex SuperTokenizer = new(@"
+(?<newline>          [\n\r]                             )| (?#Newlines...)
+(?<multilineComment> \$\*(.|\s)*?\*\$                   )| (?#Multiline Comment)
+(?<string>           (?<qoute>[\""'`])(\\.|.)*?\<qoute> )| (?#Strings)
+(?<singleineComment> \$\$.*                             )| (?#Singleline Comment)
+(?<preprocessor>     \s*\$(?<directive>\w+)(?<body>.+)  )| (?#Preprocessor)
+(?<code>             [^$\""'\r\n]+                      )| (?#Regular, Boring code)
+(?<unknown>          .                                  )  (?#Incase I missed something)",
+RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
+
     public static string Process(StreamReader reader) {
         StringBuilder sb = new();
-        string? line;
-        while((line = reader.ReadLine()) != null) {
-            if(IsDirective.IsMatch(line)) {
-                var match = IsDirective.Match(line);
-                var directive = match.Groups["directive"].Value;
-                var body = match.Groups["body"].Value;
+        string allCode = reader.ReadToEnd();
+        // For each chunk of code
+        foreach (Match m in SuperTokenizer.Matches(allCode)) {
+            // If it's a preprocessor directive
+            if (m.Groups["preprocessor"].Success) {
+                var directive = m.Groups["directive"].Value;
+                var body = m.Groups["body"].Value;
                 switch(directive) {
                     case "cInlineStart":
                         CInline.Parse(reader, sb);
@@ -21,11 +34,27 @@ public class Preprocessor {
                         Define.Parse(body);
                         break;
                     default:
-                        sb.AppendLine(line);
+                        sb.Append(m.Value);
                         break;
                 }
-            } else {
-                sb.AppendLine(line);
+            // Comments are ignored.
+            }else if(m.Groups["multilineComment"].Success || m.Groups["singleineComment"].Success) {
+                continue;
+            
+            // Strings and Newlines are left in tact
+            }else if(m.Groups["string"].Success || m.Groups["newline"].Success) {
+                sb.Append(m.Value);
+            
+            // Code or unknown is ran through the define system
+            }else if(m.Groups["code"].Success || m.Groups["unknown"].Success) {
+                string body = m.Value;
+                foreach(var define in Define.Macros) {
+                    body = define.Match.Replace(body, define.Replace);
+                }
+                sb.Append(body);
+            }else{
+                // We shouldn't be here.
+                throw new("Unknown match: " + m.Value);
             }
         }
         return sb.ToString();
